d189d1f1069574630585a2ea75fcd93b46824854
Diya K Dileep <128391245+dya8@users.noreply.github.com>
2024-10-04 22:20:58 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 2581-divide-players-into-teams-of-equal-skill/2581-divide-players-into-teams-of-equal-skill.java
@@ -0,0 +1,22 @@
+class Solution
+ {
+    public long dividePlayers(int[] skill)
+     {
+        Arrays.sort(skill); // Sort the skills in ascending order
+        int n = skill.length;
+        int totalSkill = skill[0] + skill[n - 1]; // The expected total skill for each team
+        int sumChemistry = 0;
+
+        // Create teams of two players with equal total skill
+        for (int i = 0; i < n / 2; i++)
+         {
+            int teamSkill = skill[i] + skill[n - 1 - i];
+            if (teamSkill != totalSkill) 
+            {
+                return -1; // If a team cannot have the expected total skill
+            }
+            sumChemistry += skill[i] * skill[n - 1 - i]; // Add the chemistry of the current team
+        }
+        return sumChemistry;
+}
+ }
